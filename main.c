890041af964397a7f3b4e3c8c1b9a7f7f70a9fe1
Tero Hongisto <tero.hongisto@kolumbus.fi>
2023-05-11 22:33:13 +0300

    Testing filereading and memory allocation again.

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define BUFFER_SIZE 512
+
 struct node {
     int x;
     int y;
@@ -15,6 +17,32 @@ int main(int argc,char **argv) {
     for (int c = 0 ; c < argc; c++) {
         printf("%d : %s\n",c,argv[c]);
     }
+
+    FILE *fptr;
+    fptr = fopen("random.txt","r");
+
+    if(fptr == NULL) {
+        printf("File open error!");
+        exit(1);
+    }
+
+    int* memptr;
+    memptr = (struct node*)malloc(sizeof(struct node));
+    if (memptr == NULL) {
+        printf("Memory not allocated.\n");
+        exit(0);
+    }
+    free(memptr);
+
+    int numread;
+    char buffer[BUFFER_SIZE];
+    while(!feof(fptr)) {
+        memset(buffer, 0, BUFFER_SIZE);
+        numread = fscanf(fptr, "%s", buffer);
+        buffer[BUFFER_SIZE] = 0;
+        printf("%d - %s\n",numread,buffer);
+    }
+    fclose(fptr);
 
     struct node example;
     example.x = 99;
