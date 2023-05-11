3c7fcd0e73e57f84293633b12191e2d1ea015f50
Tero Hongisto <tero.hongisto@kolumbus.fi>
2023-05-11 22:50:05 +0300

    added to do list

--- main.c
+++ main.c
@@ -5,11 +5,9 @@
 #define BUFFER_SIZE 512
 
 struct node {
-    int x;
-    int y;
-    double value;
-    struct node *prev;
-    struct node *next;
+    int value;
+    struct node *low;
+    struct node *hi;
 };
 
 int main(int argc,char **argv) {
@@ -26,13 +24,15 @@ int main(int argc,char **argv) {
         exit(1);
     }
 
-    int* memptr;
+    struct node* memptr;
     memptr = (struct node*)malloc(sizeof(struct node));
     if (memptr == NULL) {
         printf("Memory not allocated.\n");
         exit(0);
     }
     free(memptr);
+
+    struct node* treebase;
 
     int numread;
     char buffer[BUFFER_SIZE];
@@ -42,39 +42,15 @@ int main(int argc,char **argv) {
         buffer[BUFFER_SIZE] = 0;    // ensure string ends
         if (numread == 1) {
             int value = atoi(buffer);
-            printf("%d\n",value);
+            printf("%d\n",value);          
             }
+        // reserve new node 
+        // append it to current tree based on value
+
     }
     fclose(fptr);
 
     struct node example;
-    example.x = 99;
-    example.y = 29;
-    example.value = 0.32f;
-    example.prev = NULL;
-    example.next = NULL;
-
-    struct node example2;
-    example2.x = 19;
-    example2.y = 39;
-    example2.value = 1.85f;
-    example2.prev = NULL;
-    example2.next = NULL;
-
-    example.next = &example2;
-    example2.prev = &example;
-
-    printf("X: %d\n",example.x);
-    printf("Y: %d\n",example.y);
-    printf("Value: %f\n",example.value);
-    printf("Prev: %p\n",example.prev);
-    printf("Next: %p\n\n",example.next);
-
-    printf("X: %d\n",example2.x);
-    printf("Y: %d\n",example2.y);
-    printf("Value: %f\n",example2.value);
-    printf("Prev: %p\n",example2.prev);
-    printf("Next: %p\n\n",example2.next);
 
     return 0;
 }
